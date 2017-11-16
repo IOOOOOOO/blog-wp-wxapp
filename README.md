a6c64009d28266c529be9c778d650b0d7749e02f
wuyunfeng <1363321437@qq.com>
2017-11-16 18:45:24 +0800

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+# wordpress提供后台api开发小程序版博客
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,26 +2,102 @@
 //获取应用实例
 const app = getApp()
 
-const API_BASE='https://yfblog.cn/wp-json'
+const API_BASE = 'https://yfblog.cn/wp-json'
 
-const API_ROUTE='wp/v2/posts'
+const API_ROUTE = 'wp/v2/posts'
 
 Page({
-  data: {
-      entities:[]
-  },
-  //事件处理函数
+    data: {
+        entities: [],
+        embed: true,
+        total: 0,
+        totalPages: 0,
+        currentPage: 1,
+        isLoading: true,
+        isEarth: false
+    },
+    //事件处理函数
 
-  onLoad () {
-      wx.request({
-          url:`${ API_BASE }/${ API_ROUTE }`,
-          success:(response)=>{
-              console.log(response)
-              const entities =response.data
-              this.setData({
-                  entities
-              })
-          }
-      })
-  }
+    onLoad() {
+        wx.request({
+            url: `${ API_BASE }/${ API_ROUTE }?_embed=${ this.data.embed }`,
+            success: (response) => {
+                response.data.map(item => {
+                    item.excerpt['plaintext']= item.excerpt.rendered.replace(/<[^>]+>/g,"")
+                    return item;
+                })
+                const entities = response.data
+                this.setData({
+                    entities,
+                    isLoading: false,
+                    total: response.header['x-wp-total'],
+                    totalPages: response.header['x-wp-totalpages'],
+                    currentPage: 1,
+                    isEarth: false
+                })
+            }
+        })
+    },
+    onPullDownRefresh() {
+        wx.request({
+            url: `${ API_BASE }/${ API_ROUTE }?_embed=${ this.data.embed }`,
+            success: (response) => {
+                response.data.map(item => {
+                    item.excerpt['plaintext']= item.excerpt.rendered.replace(/<[^>]+>/g,"")
+                    return item;
+                })
+                const entities = response.data
+                this.setData({
+                    entities,
+                    isLoading: false,
+                    total: response.header['x-wp-total'],
+                    totalPages: response.header['x-wp-totalpages'],
+                    currentPage: 1,
+                    isEarth: false
+                })
+                wx.stopPullDownRefresh()
+            }
+        })
+    },
+
+    onReachBottom() {
+
+        let {
+            currentPage,
+            totalPages
+        } = this.data
+        if (currentPage >= totalPages) {
+            return
+        }
+
+        this.setData({
+            isLoading: true
+        })
+
+        currentPage = currentPage + 1
+
+        wx.request({
+            url: `${ API_BASE }/${ API_ROUTE }?_embed=${ this.data.embed }&page=${currentPage}`,
+            success: (response) => {
+                response.data.map(item => {
+                    item.excerpt['plaintext']= item.excerpt.rendered.replace(/<[^>]+>/g,"")
+                    return item;
+                })
+                const entities = [...this.data.entities, ...response.data]
+                this.setData({
+                    entities,
+                    currentPage,
+                    isLoading: false,
+                    total: response.header['x-wp-total'],
+                    totalPages: response.header['x-wp-totalpages'],
+                    isEarth: currentPage >= totalPages
+                })
+            }
+        })
+
+
+
+    }
+
+
 })
